63d80952e5798f7ddc58389604bd2311c5296ab5
Kaspars Dambis <hi@kaspars.net>
2021-10-27 12:32:43 +0300

    Verify the patch

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require( 'path' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+
+// Patch the default WP config to prevent removing the "stale" assets.
+defaultConfig.plugins[0] = new CleanWebpackPlugin( {
+    cleanAfterEveryBuildPatterns: [ '!fonts/**', '!images/**' ],
+    // Prevent it from deleting webpack assets during builds that have
+    // multiple configurations returned in the webpack config.
+    cleanStaleWebpackAssets: false,
+} );
 
 const configModern = {
     ...defaultConfig,
